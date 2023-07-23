221267a9f0894042340df5ff9677f789943586b6
Flavio Andrade <foa12381@gmail.com>
2023-07-23 12:02:52 -0300

    feat/navegation

--- src/components/nav2/nav2.tsx
+++ src/components/nav2/nav2.tsx
@@ -1,20 +1,32 @@
 import { useState } from 'react'
 import styles from './nav.module.css'
 import logo from '../../assets/logos/logo.png'
+//import { useLocation } from 'react-router-dom';
 
 export default function Nav2() {
+  //const url = useLocation();
+  //console.log(url);
   const [toggle, setToggle] = useState(false)
 
   function handleClick() {
     setToggle(!toggle)
     console.log(toggle, "hello word")
+
+  //   <nav className="flex items-center justify-center h-[6rem] gap-3">
+  //   <Link to='/'>
+  //     <p className={url.pathname === '/' ? styles.ative : ''}>Home</p>
+  //   </Link>
+  //   <Link to='/sobre'>
+  //     <p className={url.pathname === '/sobre' ? styles.ative : ''}>Sobre</p>
+  //   </Link>
+  // </nav>
   }
 
   return (
     <div className={styles.container}>
       <span><img src={logo} alt="logo"/></span>
       <ul className={styles.ul}>
-        <li className={styles.li}>Home</li>
+        <li>Home</li>
         <li>Palavra</li>
         <li>Devocional</li>
         <li>Historia</li>
